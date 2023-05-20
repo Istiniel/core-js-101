417b2f740c5941904465e143c6fa0075b19e520e
Istiniel <eclipsio@mail.ru>
2023-05-21 00:50:47 +0300

    feat: numbers part done

--- test.js
+++ test.js
@@ -1,16 +1,14 @@
-function encodeToRot13(str) {
-  const result = [];
-  for (let i = 0; i < str.length; i++) {
-    const unicode = str.charCodeAt(i);
-    if (unicode >= 65 && unicode <= 90) {
-      result.push(String.fromCharCode(((unicode - 65 + 13) % 26) + 65));
-    } else if (unicode >= 97 && unicode <= 122) {
-      result.push(String.fromCharCode(((unicode - 97 + 13) % 26) + 97));
-    } else {
-      result.push(str[i]);
-    }
+function toNumber(value, def) {
+  const result = Number(value);
+
+  console.log(result);
+  if (
+    !Number.isNaN(result) &&
+    Object.prototype.toString.call(result) === '[object Number]'
+  ) {
+    return result;
   }
-  return result.join('');
+  return def;
 }
 
-console.log(encodeToRot13('Why did the chicken cross the road?'));
+console.log(toNumber(null, 0));
