2c17296fd72a41a1e10922a61b416667bf72df3c
Istiniel <eclipsio@mail.ru>
2023-05-27 00:08:07 +0300

    feat: objects part done

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -19,8 +19,14 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() {
+      return this.width * this.height;
+    },
+  };
 }
 
 /**
@@ -33,8 +39,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -106,33 +112,141 @@ function fromJSON(/* proto, json */) {
  *  For more examples see unit tests.
  */
 
+class Element {
+  constructor(tag, selector) {
+    this.order = [
+      'elem',
+      'idfield',
+      'classfield',
+      'attrfield',
+      'pseudoclassfield',
+      'pseudoElem',
+    ];
+    this.currentSelectors = [];
+    this.currentSelectors.push(selector);
+    this[selector] = selector;
+    this.css = tag;
+  }
+
+  element(value) {
+    this.css += `${value}`;
+    if (this.elem) {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector',
+      );
+    }
+    this.element = value;
+    this.currentSelectors.push('elem');
+    this.checkOrder('elem');
+    return this;
+  }
+
+  id(value) {
+    this.css += `#${value}`;
+    if (this.idfield) {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector',
+      );
+    }
+    this.idfield = value;
+    this.currentSelectors.push('idfield');
+    this.checkOrder('idfield');
+    return this;
+  }
+
+  class(value) {
+    this.css += `.${value}`;
+    this.currentSelectors.push('classfield');
+    this.checkOrder('classfield');
+    return this;
+  }
+
+  attr(value) {
+    this.css += `[${value}]`;
+    this.currentSelectors.push('attrfield');
+    this.checkOrder('attrfield');
+    return this;
+  }
+
+  pseudoClass(value) {
+    this.css += `:${value}`;
+    this.currentSelectors.push('pseudoclassfield');
+    this.checkOrder('pseudoclassfield');
+    return this;
+  }
+
+  pseudoElement(value) {
+    this.css += `::${value}`;
+    if (this.pseudoElem) {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector',
+      );
+    }
+    this.pseudoElem = value;
+    this.currentSelectors.push('pseudoElem');
+    this.checkOrder('pseudoElem');
+    return this;
+  }
+
+  combine(selector1, combinator, selector2) {
+    this.css = `${selector1} ${combinator} ${selector2}`;
+    return this;
+  }
+
+  toString() {
+    return this.css;
+  }
+
+  stringify() {
+    return this.css;
+  }
+
+  checkOrder(selector) {
+    const selectors = this.order.slice(this.order.indexOf(selector) + 1);
+    selectors.forEach((elem) => {
+      if (this.currentSelectors.includes(elem)) {
+        throw new Error(
+          'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
+        );
+      }
+    });
+  }
+}
+
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  css: '',
+
+  element(value) {
+    return new Element(value, 'elem');
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    return new Element(`#${value}`, 'idfield');
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    return new Element(`.${value}`, 'classfield');
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    return new Element(`[${value}]`, 'attrfield');
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    return new Element(`:${value}`, 'pseudoclassfield');
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    return new Element(`::${value}`, 'pseudoElem');
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    this.css = `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+    return this;
+  },
+
+  stringify() {
+    return this.css;
   },
 };
 
--- test.js
+++ test.js
@@ -1,18 +1,159 @@
-function angleBetweenClockHands(timestamp) {
-  const date = new Date(timestamp - 3600 * 1000 * 3);
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const minutesDeg = (minutes * 360) / 60;
-  const hoursDeg = ((hours % 12) * 360) / 12 + (minutes * 30) / 60;
-
-  let result = Math.abs(hoursDeg - minutesDeg);
-
-  if (result > 180) {
-    result = 360 - result;
+class Element {
+  constructor(tag, selector) {
+    this.order = [
+      'elem',
+      'idfield',
+      'classfield',
+      'attrfield',
+      'pseudoclassfield',
+      'pseudoElem',
+    ];
+    this.currentSelectors = [];
+    this.currentSelectors.push(selector);
+    this[selector] = selector;
+    this.css = tag;
   }
 
-  return (result * Math.PI) / 180;
+  element(value) {
+    this.css += `${value}`;
+    if (this.elem) {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector',
+      );
+    }
+    this.element = value;
+    this.currentSelectors.push('elem');
+    this.checkOrder('elem');
+    return this;
+  }
+
+  id(value) {
+    this.css += `#${value}`;
+    if (this.idfield) {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector',
+      );
+    }
+    this.idfield = value;
+    this.currentSelectors.push('idfield');
+    this.checkOrder('idfield');
+    return this;
+  }
+
+  class(value) {
+    this.css += `.${value}`;
+    this.currentSelectors.push('classfield');
+    this.checkOrder('classfield');
+    return this;
+  }
+
+  attr(value) {
+    this.css += `[${value}]`;
+    this.currentSelectors.push('attrfield');
+    this.checkOrder('attrfield');
+    return this;
+  }
+
+  pseudoClass(value) {
+    this.css += `:${value}`;
+    this.currentSelectors.push('pseudoclassfield');
+    this.checkOrder('pseudoclassfield');
+    return this;
+  }
+
+  pseudoElement(value) {
+    this.css += `::${value}`;
+    if (this.pseudoElem) {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector',
+      );
+    }
+    this.pseudoElem = value;
+    this.currentSelectors.push('pseudoElem');
+    this.checkOrder('pseudoElem');
+    return this;
+  }
+
+  combine(selector1, combinator, selector2) {
+    this.css = `${selector1} ${combinator} ${selector2}`;
+    return this;
+  }
+
+  toString() {
+    return this.css;
+  }
+
+  stringify() {
+    return this.css;
+  }
+
+  checkOrder(selector) {
+    const selectors = this.order.slice(this.order.indexOf(selector) + 1);
+    selectors.forEach((elem) => {
+      if (this.currentSelectors.includes(elem)) {
+        throw new Error(
+          'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
+        );
+      }
+    });
+  }
 }
 
-console.log(angleBetweenClockHands(Date.UTC(2016, 3, 5, 15, 0)));
-// console.log(new Date(Date.UTC(2016, 3, 5, 9, 0)));
+const cssSelectorBuilder = {
+  css: '',
+
+  element(value) {
+    return new Element(value, 'elem');
+  },
+
+  id(value) {
+    return new Element(`#${value}`, 'idfield');
+  },
+
+  class(value) {
+    return new Element(`.${value}`, 'classfield');
+  },
+
+  attr(value) {
+    return new Element(`[${value}]`, 'attrfield');
+  },
+
+  pseudoClass(value) {
+    return new Element(`:${value}`, 'pseudoclassfield');
+  },
+
+  pseudoElement(value) {
+    return new Element(`::${value}`, 'pseudoElem');
+  },
+
+  combine(selector1, combinator, selector2) {
+    this.css = `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+    return this;
+  },
+
+  stringify() {
+    return this.css;
+  },
+};
+
+const builder = cssSelectorBuilder;
+
+const test = builder.pseudoClass('hover').attr('title');
+console.log(test);
+
+// const test1 = builder
+//   .combine(
+//     builder.element('div').id('main').class('container').class('draggable'),
+//     '+',
+//     builder.combine(
+//       builder.element('table').id('data'),
+//       '~',
+//       builder.combine(
+//         builder.element('tr').pseudoClass('nth-of-type(even)'),
+//         ' ',
+//         builder.element('td').pseudoClass('nth-of-type(even)'),
+//       ),
+//     ),
+//   )
+//   .stringify();
+// console.log(test1);
